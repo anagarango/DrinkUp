942dc4d55119f7c1159a410539ce15d553a6b90a
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-01-23 11:36:07 -0800

    styling home page in progress

--- .history/pages/index_20230123112741.js
+++ .history/pages/index_20230123112741.js
@@ -23,8 +23,7 @@ const Container = styled.div`
   width:80vw;
   display:flex;
   flex-wrap: wrap;
-  justify-content: space-around;
-  margin: ${props=>props.margin}
+  justify-content: space-around
 `
 
 
@@ -134,24 +133,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <FlexBox width="100vw" height="70px" bgColor="black" justifyContent="space-between" padding="0px 25px">
-          <Heading>DrinkUp</Heading>
-          <Input value={search} onChange={event => setSearch(event.target.value)} onKeyDown={SearchCocktail}></Input>
-        </FlexBox>
-        <FlexBox height="fit-content" width="85vw" maxWidth="830px" position="absolute" boxShadow="0px 0px 3px 4px #FFFFFF, 0px 0px 11px 5px #D255C6, 0px 0px 4px 10px rgba(210, 85, 198, 0.18), inset 0px 0px 4px 3px #FFFFFF, inset 0px 0px 11px 5px #D255C6, inset 0px 0px 4px 10px rgba(210, 85, 198, 0.18)" padding="35px" top="200px" dir="column" bgColor="rgba(0, 0, 0, 0.75);">
-            <Heading>Welcome to DrinkUp</Heading>
-            <Paragraph padding="10px 0 0 0">Simplifying your Cocktail Preferences</Paragraph>
-            <Paragraph padding="30px 0 0 0">DrinkUp, a pain-point that became a project, is inspired to help new-legal aged people to find their favourite cocktails to order in their next pub, bar, and/or nightclub visit. Drink Responsibly!</Paragraph>
-        </FlexBox>
-        
-        
-        
-        <FlexBox dir="column" height="fit-content" justifyContent="flex-end">
-          <Image src="/Hero.jpeg" minHeight="65vh" width="100vw"></Image>
-          <Image src="/SecondHero.jpeg" minHeight="calc(100px + 40vh)" width="100vw" margin="-5px 0 0 0" bgPosition="50% 15%"></Image>
-          <Image position="absolute" src="/Hero2.png" width="100vw"></Image>
-          
-        </FlexBox>
+        z
         
       <FlexBox>
           {listOfIngredients.map((o)=>(
