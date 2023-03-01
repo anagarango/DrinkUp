1cc200ea981c6f80a92bb56a62c13461c2eaa627
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-02-28 21:21:56 -0800

    youtube link and activity api not working on vercel

--- next.config.js
+++ next.config.js
@@ -23,6 +23,37 @@
 //   }
 // };
 
+// module.exports = {
+//   // reactStrictMode: true,
+//   // compiler:{
+//   //   styledComponents:true
+//   // },
+//   async headers() {
+//     return [
+//       {
+//         // Allow loading of resources from googleads.g.doubleclick.net
+//         source: 'https://googleads.g.doubleclick.net/:path*',
+//         headers: [
+//           {
+//             key: 'Content-Security-Policy',
+//             value: 'frame-ancestors https://www.youtube.com',
+//           },
+//         ],
+//       },
+//       {
+//         // Allow loading of resources from static.doubleclick.net
+//         source: 'https://static.doubleclick.net/:path*',
+//         headers: [
+//           {
+//             key: 'Content-Security-Policy',
+//             value: 'frame-ancestors https://www.youtube.com',
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// }
+
 module.exports = {
   reactStrictMode: true,
   compiler:{
@@ -31,15 +62,24 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://www.youtube.com"
-          }
-        ]
-      }
-    ]
-  }
-}
+            key: "Content-Security-Policy",
+            value: "frame-ancestors https://www.youtube.com",
+          },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
 
