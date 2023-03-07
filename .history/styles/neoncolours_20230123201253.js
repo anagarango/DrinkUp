435ae49fa60383e125600e05894abf4d96f2c0b7
RenataDzotova <rdzotova@my.bcit.ca>
2023-03-07 14:33:07 -0800

    deleted history

--- .history/styles/neoncolours_20230123201253.js
+++
@@ -1,3 +0,0 @@
-export const neonColours = {
-    blueText:"0 0 0.005em #fff, 0 0 0.015em rgb(105, 251, 255),0 0 0.1em rgb(0, 207, 214), 0 0 0.15em rgb(0, 207, 214), 0 0 0.45em rgb(0, 207, 214), 0 0 0.12em"
-}
--- .history/styles/neoncolours_20230123201737.js
+++
@@ -1,4 +0,0 @@
-export const neonColours = {
-    blueText:"0 0 0.005em #fff, 0 0 0.015em rgb(105, 251, 255),0 0 0.1em rgb(0, 207, 214), 0 0 0.15em rgb(0, 207, 214), 0 0 0.45em rgb(0, 207, 214), 0 0 0.12em",
-    blueBox:"0px 0px 2px 4px #FFFFFF, 0px 0px 10px 4px #00C5CC, 0px 0px 3px 9px rgba(0, 247, 254, 0.18), inset 0px 0px 3px 2px #FFFFFF, inset 0px 0px 10px 4px #00C5CC, inset 0px 0px 3px 9px rgba(0, 247, 254, 0.18)"
-}
