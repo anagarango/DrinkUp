ac5ec4a37358bba7b28c4b4ff4d366077b98c318
RenataDzotova <rdzotova@my.bcit.ca>
2023-03-07 13:45:02 -0800

    frontend of the cocktail page

--- comps/selectedResult.js
+++ comps/selectedResult.js
@@ -20,7 +20,7 @@ export default function SelectedResult({
     },[])
         
     return(
-        <FlexBox boxShadow={neonColours.pinkBox} dir="column" alignItems="flex-start" width="90vw" maxWidth="850px" height="fit-content" bgColor="rgba(0, 0, 0, 0.65)" padding="3em">
+        <FlexBox boxShadow={neonColours.pinkBox} dir="column" alignItems="flex-start" width="90vw" maxWidth="850px" height="fit-content" bgColor="rgba(0, 0, 0, 0.65)" padding="3em" margin="120px 0 0 0">
             <FlexBox>
                 <Heading textShadow={neonColours.pinkText} fontSize="32px">{cardy.strDrink}</Heading>
                 <li style={{color:"white", fontSize:"18px", marginLeft:"20px"}}>{cardy.strAlcoholic}</li>
@@ -34,32 +34,33 @@ export default function SelectedResult({
                 }
             </FlexBox>
 
-            <FlexBox alignItems="flex-start" width="100%">
-                <FlexBox dir="column" alignItems="flex-start" width="40%">
-                    <Text color="white" fontWeight="bold" fontSize="18px" >Ingredients:</Text>
-                    <FlexBox padding="5px 0 0 15px">
-                        <FlexBox dir="column" justifyContent="flex-start" alignItems="flex-start" minHeight="60px" padding="0 7px 0 0">
+            <FlexBox dir="column" alignItems="flex-start" justifyContent="flex-start" width="100%" flexWrap="wrap">
+                <FlexBox dir="column" alignItems="flex-start" width="100%">
+                    <Text color="#f3b6f3" fontWeight="bold" fontSize="18px" >Ingredients:</Text>
+                    <FlexBox padding="10px 0 20px 0px" alignItems="flex-start">
+                        <FlexBox dir="column" justifyContent="flex-start" alignItems="flex-start" minHeight="60px" padding="0 12px 0 0">
                             {measurementArray.map((measurement, index) => (
                                 <li key={index} style={{color:"white", fontSize:"16px"}}>{measurement}</li>
                             ))}
                         </FlexBox>
                         
-                        <FlexBox dir="column" justifyContent="flex-start" alignItems="flex-start" minHeight="60px">
+                        <FlexBox dir="column" justifyContent="flex-start" alignItems="flex-start" minHeight="60px" >
                             {ingredientArray.map((ingredient, index) => (
-                                <li key={index} style={{color:"white", fontSize:"16px", listStyle: "none"}} >{ingredient}</li>
+                                <li key={index} style={{color:"white", fontSize:"16px", listStyle: "none"}}>{ingredient}</li>
                             ))}
                         </FlexBox>
                     </FlexBox>
                 </FlexBox>
                 
-                <FlexBox dir="column" alignItems="flex-start" width="60%" >
-                    <Text color="white" fontWeight="bold" fontSize="18px">Instructions:</Text>
-                    <Text padding="5px 0 0 15px" color="white" fontSize="16px">{cardy.strInstructions}</Text>
+                <FlexBox dir="column" alignItems="flex-start">
+                    <Text color="#f3b6f3" fontWeight="bold" fontSize="18px">Instructions:</Text>
+                    <Text padding="10px 0 20px 0px" color="white" fontSize="16px">{cardy.strInstructions}</Text>
                 </FlexBox>
             </FlexBox>
-
-            <Text width="100%" color="white" fontWeight="bold" fontSize="18px">Fun Activity to try after your first drink (unless it's unsafe or unlawful to do so) :</Text>
-            <Text padding="5px 0 0 15px" color="white" fontSize="16px">{activity}</Text>
+            <FlexBox width="100%" flexWrap="wrap" justifyContent="flex-start">
+                <Text color="#f3b6f3" fontWeight="bold" fontSize="18px" padding="0 10px 0 0">Fun Activity to try after your first drink: </Text>
+                <Text color="white" fontSize="16px" padding="3px 0 0 0">{activity}</Text>
+            </FlexBox>
         </FlexBox>
     )
 }
